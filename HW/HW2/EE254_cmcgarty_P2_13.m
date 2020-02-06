365d752e0104230b0e1794a745d7d8717e95154f
Connor McGarty <cmcgarty@uab.edu>
2020-02-06 01:10:33 -0600

    end of 2/5/20 work

---
+++ HW/HW2/EE254_cmcgarty_P2_13.m
@@ -0,0 +1,17 @@
+% If a force 
+% $F(N)$
+% is applied to compress a spring, its displacement
+% $x(m)$
+% can often be modeled by Hooke's law:
+%
+% $$F=kx$$
+%
+% where 
+% $k=$
+% the spring constant (N/m). The potential energy stored in the spring
+% $U(J)$
+% can then be computed as
+%
+% $$U=\frac{1}{2}kx^2$$
+%
+% Five springs are tested and the following data compiled:
